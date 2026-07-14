6562eb7660a0416104d3f65820950efbf27451af
Hodge <3135713645@qq.com>
2026-07-14 09:01:49 +0800

    Update top.v

--- sync/top.v
+++ sync/top.v
@@ -1,1 +1,16 @@
-
+//同步
+module sync_rst(
+input d,
+input clk,
+input reset,
+input set,
+output reg q
+    );
+  always@(posedge clk)begin
+  if(!reset)q<=1'b0;
+  else if(!set)q<=1'b1;
+  else q<=d;
+  
+  end
+    
+endmodule
